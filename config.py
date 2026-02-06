a5577c3531a298d3502fe1be3b54c8be438637a2
2970043992zxy-ux <2970043992zxy@gmail.com>
2026-02-06 13:13:02 +0800

    config.py
    参数文件

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 """配置管理"""
 
 # 真实API配置 - 使用你的Active Key!
-API_KEY = '6ac6805e8c67ab3ce2b811aa35278277'  # 用这个已经激活的Key
+API_KEY = 'YOUR API-KEY'  # 用已经激活的Key
 BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
 CITY_LOCATION = 'London,uk'
 UNITS = 'metric'
